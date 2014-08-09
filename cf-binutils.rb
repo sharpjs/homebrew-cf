d9a3b33bbebdefd7ab45d3f29af981f18b04a421
Jeff Sharp <jeff.sharp@git>
2014-08-09 16:48:31 -0500

    Rename to cf-binutils. Fix configure options and file conflicts.

--- cf-binutils.rb
+++ cf-binutils.rb
@@ -1,28 +1,25 @@
 require 'formula'
 
-class Mcf5307Binutils < Formula
+class CfBinutils < Formula
   homepage 'http://www.gnu.org/software/binutils/binutils.html'
   url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
   sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
 
   def install
-    system "./configure", "--program-prefix=mcf5307-",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--infodir=#{info}",
-                          "--mandir=#{man}",
+                          "--oldincludedir=#{prefix}/include",
+                          "--program-prefix=cf-",
                           "--target=m68k-none-elf",
-                          "--with-arch=cf",
-                          "--with-cpu=5307",
-                          "--enable-multilib",
+                          "--disable-nls",
                           "--disable-werror"
     system "make"
     system "make check"
     system "make install"
 
     # Remove files that conflict with Homebrew binutils
-    rm_rf include
-    rm_rf lib
-    rm_rf share
+    rm_rf share/info
   end
 end
