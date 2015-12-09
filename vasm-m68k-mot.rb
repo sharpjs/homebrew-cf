33694ca35992c0f7cdd16507fce810441a35fd1b
Jeff Sharp <jeff.sharp@git>
2015-12-08 19:32:05 -0600

    vasm-m68k-mot 1.7d

--- vasm-m68k-mot.rb
+++ vasm-m68k-mot.rb
@@ -1,24 +1,26 @@
 require "formula"
 
 class VasmM68kMot < Formula
+  desc "portable and retargetable assembler"
   homepage "http://sun.hasenbraten.de/vasm/"
-  url "http://sun.hasenbraten.de/vasm/release/vasm.tar.gz"
-  version "1.7b"
-  sha1 "40a25d82fcdf1f8e17f8103bb06075780b7866a2"
+  url "http://server.owl.de/~frank/tags/vasm1_7d.tar.gz"
+  version "1.7d"
+  sha256 "bb604d1ee65e28753aa8890ca99b4464eb7f74340e702bc9b2adf31a67cb860b"
 
   def install
     inreplace "Makefile" do |s|
-      s.change_make_var! "COPTS",   "-c #{ENV.cflags}"
+      s.change_make_var! "COPTS", "-c #{ENV.cflags}"
       s.change_make_var! "LDFLAGS", "-lm #{ENV.ldflags}"
     end
 
-    cpu = "m68k"
+    cpu    = "m68k"
     syntax = "mot"
+    vasm   = "vasm#{cpu}_#{syntax}"
 
-    system "make", "CPU=#{cpu}", "SYNTAX=#{syntax}", "vasm#{cpu}_#{syntax}"
-    bin.install "vasm#{cpu}_#{syntax}"
-
+    system "make", "CPU=#{cpu}", "SYNTAX=#{syntax}", vasm
     system "make", "vobjdump"
+
+    bin.install vasm
     bin.install "vobjdump"
   end
 end
