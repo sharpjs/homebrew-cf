c7bfcaee67f1325695e6d2f10003cf7717f201eb
Jeff Sharp <jeff.sharp@git>
2016-11-07 06:02:48 -0600

    vasm-m68k-mot 1.7g

--- vasm-m68k-mot.rb
+++ vasm-m68k-mot.rb
@@ -1,11 +1,11 @@
 require "formula"
 
 class VasmM68kMot < Formula
-  desc "Portable and retargetable assembler"
+  desc     "Portable and retargetable assembler"
   homepage "http://sun.hasenbraten.de/vasm/"
-  url "http://server.owl.de/~frank/tags/vasm1_7e.tar.gz"
-  version "1.7e"
-  sha256 "2878c9c62bd7b33379111a66649f6de7f9267568946c097ffb7c08f0acd0df92"
+  url      "http://server.owl.de/~frank/tags/vasm1_7g.tar.gz"
+  version  "1.7g"
+  sha256   "9cfca33c348fe10419cb2ef59a7ff25c262cd6cf1d353bb51d4468f1f1535c55"
 
   def install
     cpu    = "m68k"
